f7471960e6f795427ddb1354a680769c3981e384
xodiumluma <28709874+xodiumluma@users.noreply.github.com>
2023-04-17 21:59:18 +1000

    [learn] assume policy

--- __learn__/examples/eks-getting-started/eks-cluster.tf
+++ __learn__/examples/eks-getting-started/eks-cluster.tf
@@ -5,4 +5,16 @@
 
 resource "aws_iam_role" "demo-cluster" {
   name = "terraform-eks-demo-cluster"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    "Effect": "Allow",
+    "Principal": {
+      "Service": "eks.amazonaws.com"
+    },
+    "Action": "sts:AssumeRole"
+  ]
+}
+POLICY
 }
